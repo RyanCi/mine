ab3e6139d776b55d31cb3b62aee5c0d3ee57d19c
Ainurrochman <yakuza1308@gmail.com>
2017-09-19 13:40:43 +0800

    Add files via upload

---
+++ new_acces.js
@@ -0,0 +1,18 @@
+db.acl_access.save({
+    "_id" : "METRICCOUNTRYANALYSIS",
+    "id" : "METRICCOUNTRYANALYSIS",
+    "title" : "Metric Country Analysis",
+    "category" : 5,
+    "icon" : "",
+    "parentid" : "DASHBOARD",
+    "url" : "#",
+    "index" : 1,
+    "group1" : "",
+    "group2" : "",
+    "group3" : "",
+    "enable" : true,
+    "specialaccess1" : "",
+    "specialaccess2" : "",
+    "specialaccess3" : "",
+    "specialaccess4" : ""
+})
